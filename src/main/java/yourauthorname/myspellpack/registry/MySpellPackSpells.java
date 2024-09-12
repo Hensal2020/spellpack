4fb40b6fc73fcf1e162fd41310f8fd9c9b7692d2
Hensal2024 <ObsidianLiebhaber@protonmail.com>
2024-09-12 17:59:19 +0200

    Initial commit

---
+++ src/main/java/yourauthorname/myspellpack/registry/MySpellPackSpells.java
@@ -0,0 +1,48 @@
+package yourauthorname.myspellpack.registry;
+
+import electroblob.wizardry.entity.projectile.EntityIceShard;
+import electroblob.wizardry.spell.Spell;
+import electroblob.wizardry.spell.SpellArrow;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+import net.minecraftforge.registries.IForgeRegistry;
+import yourauthorname.myspellpack.MySpellPack;
+import yourauthorname.myspellpack.spell.BloodStep;
+import yourauthorname.myspellpack.spell.ExplodeMyself;
+import yourauthorname.myspellpack.spell.MarkThyself;
+
+import javax.annotation.Nonnull;
+
+
+
+@ObjectHolder(MySpellPack.MODID)
+@EventBusSubscriber
+public final class MySpellPackSpells {
+
+	private MySpellPackSpells() {} // no instances
+
+
+	public static final Spell explode_myself = placeholder();
+	public static final Spell mark_thyself = placeholder();
+	public static final Spell blood_step = placeholder();
+	//public static final Spell blood_shard = placeholder();
+
+	@Nonnull
+	@SuppressWarnings("ConstantConditions")
+	private static <T> T placeholder() {return null;}
+
+	@SubscribeEvent
+	public static void register(RegistryEvent.Register<Spell> event) {
+
+		IForgeRegistry<Spell> registry = event.getRegistry();
+
+		// 1.0.0 Spells
+		registry.register(new ExplodeMyself());
+		registry.register(new MarkThyself());
+		registry.register(new BloodStep());
+		//registry.register(new SpellArrow<>("blood_shard", EntityBloodShard::new).addProperties(Spell.DAMAGE,Spell.EFFECT_DURATION,Spell.EFFECT_STRENGTH));
+
+	}
+}
---
+++ src/main/java/yourauthorname/myspellpack/spell/MarkThyself.java
@@ -0,0 +1,27 @@
+package yourauthorname.myspellpack.spell;
+
+import electroblob.wizardry.item.SpellActions;
+import electroblob.wizardry.spell.Spell;
+import electroblob.wizardry.util.SpellModifiers;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+import yourauthorname.myspellpack.MySpellPack;
+import yourauthorname.myspellpack.registry.MySpellPackPotions;
+
+public class MarkThyself extends Spell{
+
+    public MarkThyself(){
+        super(MySpellPack.MODID, "mark_thyself", SpellActions.POINT_UP,false);
+        //addproperties
+    }
+
+    @Override
+    public boolean cast(World world, EntityPlayer caster, EnumHand hand, int ticksInUse, SpellModifiers modifiers){
+        caster.attackEntityFrom(DamageSource.MAGIC, 1);
+        caster.addPotionEffect(new PotionEffect(MySpellPackPotions.bmarked,200,0));
+        return true;
+    }
+}
